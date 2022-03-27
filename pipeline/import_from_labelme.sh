72ee576209a500a8555e8ae24342f17c750d083a
Evgeny Toropov <toropov.evgeny@gmail.com>
2022-03-27 14:29:16 -0400

    Fixed broken pipeline/import_from_labelme.sh

--- pipeline/import_from_labelme.sh
+++ pipeline/import_from_labelme.sh
@@ -16,8 +16,8 @@ Usage:
 
 Example:
   $PROGNAME
-     --campaign_id 7
-     --out_version 5
+     --campaign_id 8
+     --out_version 7
 
 Options:
   --campaign_id
@@ -77,9 +77,11 @@ if [ -z "$out_version" ]; then
   echo "Argument 'out_version' is required."
   exit 1
 fi
+previous_campaign_id=$((campaign_id-1))
 
-echo "campaign_id:  ${campaign_id}"
-echo "out_version:  ${out_version}"
+echo "campaign_id:           ${campaign_id}"
+echo "previous_campaign_id:  ${previous_campaign_id}"
+echo "out_version:           ${out_version}"
 
 # The end of the parsing code.
 ################################################################################
@@ -93,20 +95,21 @@ conda activate ${CONDA_SHUFFLER_ENV}
 echo "Conda environment is activated: '${CONDA_SHUFFLER_ENV}'"
 
 shuffler_bin=${SHUFFLER_DIR}/shuffler.py
-cd ${SHUFFLER_DIR}
-git checkout "stamps"
-cd -
 
-db_1800x1200_path=$(get_1800x1200_db_path ${campaign_id} ${out_version})
-db_6Kx4K_path=$(get_6Kx4K_db_path ${campaign_id} ${out_version})
+in_db_1800x1200_uptoprevious_path=$(get_1800x1200_uptonow_db_path ${previous_campaign_id} "latest")
+in_db_6Kx4K_uptoprevious_path=$(get_6Kx4K_uptonow_db_path ${previous_campaign_id} "latest")
+out_db_1800x1200_path=$(get_1800x1200_db_path ${campaign_id} ${out_version})
+out_db_6Kx4K_path=$(get_6Kx4K_db_path ${campaign_id} ${out_version})
+out_db_1800x1200_uptonow_path=$(get_1800x1200_uptonow_db_path ${campaign_id} ${out_version})
+out_db_6Kx4K_uptonow_path=$(get_6Kx4K_uptonow_db_path ${campaign_id} ${out_version})
 
 # If this script was already run before, the output db exists, and Shuffler will raise an exception.
-rm -f ${db_1800x1200_path}
+rm -f ${out_db_1800x1200_path} ${out_db_6Kx4K_path} ${out_db_1800x1200_uptonow_path} ${out_db_6Kx4K_uptonow_path}
 
 ${shuffler_bin} \
   --logging 30 \
   --rootdir ${ROOT_DIR} \
-  -o ${db_1800x1200_path} \
+  -o ${out_db_1800x1200_path} \
   importLabelme \
     --images_dir "${LABELME_DIR}/campaign${campaign_id}/initial-labeled/Images" \
     --annotations_dir "${LABELME_DIR}/campaign${campaign_id}/initial-labeled/Annotations" \| \
@@ -115,7 +118,7 @@ ${shuffler_bin} \
     --rootdir_for_validation "${ROOT_DIR}" \
     --subfolder_list_path "${PROJECT_DIR}/shared/data/subfolder_list.txt"
 
-sqlite3 ${db_1800x1200_path} "
+sqlite3 ${out_db_1800x1200_path} "
   UPDATE objects SET name = CAST(name AS TEXT); 
   UPDATE objects SET name = REPLACE(name, '-', '');
   UPDATE objects SET name = REPLACE(name, '.', '');
@@ -123,33 +126,33 @@ sqlite3 ${db_1800x1200_path} "
 "
 
 ${shuffler_bin} \
-  -i ${db_1800x1200_path} -o ${db_1800x1200_path} --logging 30 \
+  -i ${out_db_1800x1200_path} -o ${out_db_1800x1200_path} --logging 30 \
   extractNumberIntoProperty --property "number" \| \
   filterObjectsInsideCertainObjects \
     --where_shadowing_objects "SELECT objectid WHERE name IN ('page_rb','page_lb', 'pagerb','pagelb')"
 
 # Add the "campaign" property.
-sqlite3 ${db_1800x1200_path} \
+sqlite3 ${out_db_1800x1200_path} \
   "INSERT INTO properties(objectid,key,value) SELECT objectid,'campaign',${campaign_id} FROM objects"
 
 # Get the same db but with big images.
 ${shuffler_bin} \
-  -i ${db_1800x1200_path} -o ${db_6Kx4K_path} --rootdir ${ROOT_DIR} --logging 30 \
+  -i ${out_db_1800x1200_path} -o ${out_db_6Kx4K_path} --rootdir ${ROOT_DIR} --logging 30 \
   moveMedia --image_path "original_dataset" --level 2 --adjust_size
 
 # Merge 1800x1200 with the previous campaign.
 echo "Merging 1800x1200 with the previous campaign..."
 ${shuffler_bin} \
-  -i ${db_1800x1200_path} \
-  -o $(get_1800x1200_uptonow_db_path ${campaign_id} ${in_version}) \
-  addDb --db_file $(get_1800x1200_uptonow_db_path ${previous_campaign_id} "latest")
+  -i ${out_db_1800x1200_path} \
+  -o ${out_db_1800x1200_uptonow_path} \
+  addDb --db_file ${in_db_1800x1200_uptoprevious_path}
 
 # Merge 6Kx4K with the previous campaign.
 echo "Merging 6Kx4K with the previous campaign..."
 ${shuffler_bin} \
-  -i ${db_6Kx4K_path} \
-  -o $(get_6Kx4K_uptonow_db_path ${campaign_id} ${in_version}) \
-  addDb --db_file $(get_6Kx4K_uptonow_db_path ${previous_campaign_id} "latest")
+  -i ${out_db_6Kx4K_path} \
+  -o ${out_db_6Kx4K_uptonow_path} \
+  addDb --db_file ${in_db_6Kx4K_uptoprevious_path}
 
 log_db_version ${campaign_id} ${out_version} "Imported from labelme."
 echo "Done."
