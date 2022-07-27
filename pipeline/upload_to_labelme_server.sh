3fceac74870f6bbc9e7b812ca01441db251eaa39
julianx <julianx@gmail.com>
2022-07-27 07:30:57 -0400

    Adding WIP changes for labelme

--- pipeline/upload_to_labelme_server.sh
+++ pipeline/upload_to_labelme_server.sh
@@ -96,7 +96,53 @@ dir_to_upload_from="${LABELME_DIR}/${name}"
 echo "Will upload from directory: '${dir_to_upload_from}'"
 
 ###    PUT YOUR CODE HERE    ###
+labelme_server_full_path="/var/www/html/LabelMeAnnotationTool"
+full_campaign_name="campaign${campaign_id}/${name}"
+source_dir_full_path=${dir_to_upload_from}/${full_campaign_name}
+target_dir_full_path=${labelme_server_full_path}/${full_campaign_name}
+annotations_folder_name="Annotations"
+images_folder_name="Images"
+
+# check dir exists ${source_dir_full_path}/Annotations
+# check dir exists ${source_dir_full_path}/Images
+  check_dirs_exist ${source_dir_full_path}
+
+# check number of files are the same ${source_dir_full_path}/Annotations, ${source_dir_full_path}/Images
+  check_equal_number_of_files  # ${source_dir_full_path}
+
+# copy contents ${source_dir_full_path}/Images ${target_dir_full_path}/Images
+# copy contents ${source_dir_full_path}/Annotations ${target_dir_full_path}/Annotations
+  copy_contents
+
+change JPG extensions in Annotations files to lowercase
+change directory tag in Annotations files
+set permissions to apache-writable
 
 
+function check_dirs_exist() {
+  if [ -d ${source_dir_full_path}/${annotations_folder_name} && -d ${source_dir_full_path}/${images_folder_name} ]
+  then
+    echo "Reading content from directory ${source_dir_full_path}."
+    return 0
+  else
+    echo "Error: Directory ${source_dir_full_path} does not exists."
+    return 1
+  fi
+}
 
+function check_equal_number_of_files() {
+  number_of_files_annotations=$(ls ${source_dir_full_path}/Annotations | wc -l)
+  number_of_files_images=$(ls ${source_dir_full_path}/Images | wc -l)
+  return [[ ${number_of_files_annotations} -eq ${number_of_files_images ]]
+}
+
+function copy_contents() {
+  cp -rfpv ${source_dir_full_path}/${annotations_folder_name} ${target_dir_full_path}/${annotations_folder_name}/
+  cp -rfpv ${source_dir_full_path}/${images_folder_name}/*jpg ${target_dir_full_path}/${images_folder_name}/
+}
+
+function annotations_JPG_to_lowercase() {
+  for i in ${target_dir_full_path}/${images_folder_name}/*JPG; do mv "${i}" "${i%.*}".jpg; done
+  for i in ${target_dir_full_path}/${annotations_folder_name}/*xml; do sed -i -e 's_<folder>.*</folder>_<folder>${full_campaign_name}</folder>_' -e 's_.JPG_.jpg_' "${i}"; done
+}
 echo "Done."
