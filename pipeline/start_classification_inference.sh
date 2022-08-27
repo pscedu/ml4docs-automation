c3ef13220b84a5df00921b8eb07f5f5c93da9c22
Evgeny Toropov <toropov.evgeny@gmail.com>
2022-08-27 14:45:26 -0400

    Classification inference takes model_campaign_id on input.

--- pipeline/start_classification_inference.sh
+++ pipeline/start_classification_inference.sh
@@ -14,6 +14,7 @@ Usage:
      --campaign_id CAMPAIGN_ID
      --in_version IN_VERSION
      --out_version OUT_VERSION
+     --model_campaign_id MODEL_CAMPAIGN_ID
      --set_id SET_ID
      --run_id RUN_ID
      --dry_run_submit DRY_RUN_SUBMIT
@@ -29,7 +30,11 @@ Options:
   --in_version
       (required) The version suffix of the input database.
   --out_version
-      (required) The version of the output database. The default is in_version+1.
+      (required) The version of the output database. 
+      The default is in_version+1.
+  --model_campaign_id
+      (optional) The version of the campaign with trained model. 
+      The default is campaign_id-1.
   --set_id
       (optional) Which set of models to use for the inference.
   --run_id
@@ -43,6 +48,7 @@ ARGUMENT_LIST=(
     "campaign_id"
     "in_version"
     "out_version"
+    "model_campaign_id"
     "set_id"
     "run_id"
     "dry_run_submit"
@@ -78,6 +84,10 @@ while [[ $# -gt 0 ]]; do
             ;;
         --out_version)
             out_version=$2
+            shift 2
+            ;;
+        --model_campaign_id)
+            model_campaign_id=$2
             shift 2
             ;;
         --set_id)
@@ -116,13 +126,15 @@ if [ -z "$out_version" ]; then
   out_version=$((in_version+1))
   echo "Automatically setting out_version to ${out_version}."
 fi
-
-previous_campaign_id=$((campaign_id-1))
+if [ -z "$model_campaign_id" ]; then
+  model_campaign_id=$((campaign_id-1))
+  echo "Automatically setting model_campaign_id to ${model_campaign_id}."
+fi
 
 echo "campaign_id:            ${campaign_id}"
-echo "previous_campaign_id:   ${previous_campaign_id}"
 echo "in_version:             ${in_version}"
 echo "out_version:            ${out_version}"
+echo "model_campaign_id:      ${model_campaign_id}"
 echo "set_id:                 ${set_id}"
 echo "run_id:                 ${run_id}"
 echo "dry_run_submit:         ${dry_run_submit}"
@@ -137,7 +149,7 @@ source ${dir_of_this_file}/../constants.sh
 ${dir_of_this_file}/../scripts/classification_inference/submit.sh \
   --in_db_file "$(get_cropped_db_path ${campaign_id} ${in_version}.${set_id})" \
   --out_db_file "$(get_cropped_db_path ${campaign_id} ${out_version}.${set_id})" \
-  --model_campaign_id ${previous_campaign_id} \
+  --model_campaign_id ${model_campaign_id} \
   --set_id ${set_id} \
   --run_id ${run_id} \
   --dry_run ${dry_run_submit}
